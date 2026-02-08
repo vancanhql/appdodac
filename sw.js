42f0f01696ed8b6261eada5f05b535a522f247e8
vancanhql <canhpro.9x@gmail.com>
2026-02-08 11:16:42 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,7 @@
-const APP_VERSION = "1.0.2";
-const CACHE_NAME = "dodac-" + APP_VERSION;
+const APP_VERSION = "1.0.5";
 
 self.addEventListener("install", e => self.skipWaiting());
 
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
   self.clients.claim();
 });
